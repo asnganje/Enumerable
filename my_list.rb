3faa909037dd7dea75e5e3ef1c0dec06e1faa7e7
asnganje <abd.nganje@gmail.com>
2023-01-02 08:25:39 +0300

    add myEnumerable

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,17 @@
-class MyList4aq2
-    
+require_relative 'my_enumerable'
+
+class MyList
+    include MyEnumerable
+
+    def initialize(*array)
+    @list = array
+    end
+    def each(&block)
+        @list.each(&block)
+    end
 end
+
+list = MyList.new(1,2,3,4,5)
+puts(list.all? { |e| e < 5 })
+puts (list.any? {|e| e == 5})
+puts (list.filter {|e| e.odd?})
